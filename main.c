83d4ae8113edd9aa5c890b18ccd116955fab2aee
Lon Hohberger <lhh@redhat.com>
2018-04-12 15:22:57 -0400

    all: fix CR/LF (part 2)
    Signed-off-by: Lon Hohberger <lhh@redhat.com>

--- main.c
+++ main.c
@@ -1,37 +1,34 @@
 #include "socket.h"
 #include "irc.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-   irc_t irc;
+	irc_t irc;
 
-   if ( irc_connect(&irc, "irc.freenode.org", "6667") < 0 )
-   {
-      fprintf(stderr, "Connection failed.\n");
-      goto exit_err;
-   }
+	if (irc_connect(&irc, "devserv.devel.redhat.com", "6667") < 0) {
+		fprintf(stderr, "Connection failed.\n");
+		goto exit_err;
+	}
 
-   irc_set_output(&irc, "/dev/stdout");
+	irc_set_output(&irc, "/dev/stdout");
 
-   if ( irc_login(&irc, "maister_") < 0 )
-   {
-      fprintf(stderr, "Couldn't log in.\n");
-      goto exit_err;
-   }
+	if (irc_login(&irc, "maister_") < 0) {
+		fprintf(stderr, "Couldn't log in.\n");
+		goto exit_err;
+	}
 
-   if ( irc_join_channel(&irc, "#umgz") < 0 )
-   {
-      fprintf(stderr, "Couldn't join channel.\n");
-      goto exit_err;
-   }
-   
-   while ( irc_handle_data(&irc) >= 0 );
+	if (irc_join_channel(&irc, "#umgz") < 0) {
+		fprintf(stderr, "Couldn't join channel.\n");
+		goto exit_err;
+	}
 
-   irc_close(&irc);
-   return 0;
+	while (irc_handle_data(&irc) >= 0) ;
 
-exit_err:
-   irc_close(&irc);
-   exit(1);
+	irc_close(&irc);
+	return 0;
+
+      exit_err:
+	irc_close(&irc);
+	exit(1);
 }
-
--- socket.h
+++ socket.h
@@ -3,9 +3,9 @@
 
 #include <stdlib.h>
 
-int get_socket(const char* host, const char* port);
-int sck_send(int socket, const char* data, size_t size);
-int sck_sendf(int socket, const char* fmt, ...);
-int sck_recv(int socket, char* buffer, size_t size);
+int get_socket(const char *host, const char *port);
+int sck_send(int socket, const char *data, size_t size);
+int sck_sendf(int socket, const char *fmt, ...);
+int sck_recv(int socket, char *buffer, size_t size);
 
 #endif
